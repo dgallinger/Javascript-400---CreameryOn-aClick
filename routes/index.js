be85d4e4a32bcbf6197fe251b9f84e2893159dc4
supriyapuri <supriyapuri11@gmail.com>
2020-08-24 16:40:43 -0700

    Working shopping cart

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
-
+const Item = require('../models/items');
+const Order= require('../models/order')
 
 router.use("/user", require("./user"));
 
@@ -14,8 +15,30 @@ router.use("/contact", function(req,res,next){
     res.render('shop/contact')
 })
 
-router.use("/add-to-cart/:id", function(req,res,next){
-    const productId = req.params.id;
+router.use("/order/:id", function(req,res,next){
+    const itemId = req.params.id; 
+    let cart = new Order(req.session.cart ? req.session.cart: {});
+
+    Item.findById(itemId, function(err, item){
+        if (err){
+            return res.redirect('/');
+        }
+        cart.add(item, item.id);
+        req.session.cart = cart;
+        res.redirect('/');
+        console.log(req.session.cart)
+    });
+
+
+});
+
+
+router.use("/shopping-cart", function(req,res,next){
+    if(!req.session.cart){
+        return res.render('shop/shopping-cart', {items: null});
+    }
+    let cart = new Order(req.session.cart);
+    res.render('shop/shopping-cart', {items: cart.generateArray(), totalPrice: cart.totalPrice});
 })
 
 
