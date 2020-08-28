661e1202f8d446e78ffdb4047f5d5c8715ff59f2
Leslie <63173018+noon-coffee@users.noreply.github.com>
2020-08-28 09:50:49 -0700

    added Order functionality. route and model code commented out pending UI integration.

--- models/order.js
+++ models/order.js
@@ -8,4 +8,61 @@ const orderSchema =  new mongoose.Schema({
     
 });
 
+
+/*
+const orderSchema = new mongoose.Schema({
+  // PK: _id
+
+  // FK to User
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'users', 
+    required: true 
+  },
+
+  // Array of Item Detail
+  items: [{
+    // PK: _id
+
+    // FK to Item
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'items',
+      required: true,
+    },
+
+    // Item quantity
+    quantity: {
+      type: Number, 
+      required: true,
+    } 
+  }],
+
+  // Order total amount
+  total: { 
+    type: Number, 
+    required: true 
+  },
+
+  // Order status
+  status: {
+    type: String,
+    enum: ['New', 'Packed', 'Shipped', 'Delivered', 'Cancelled'],
+    required: true,
+  }, 
+
+  // Order create date
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+
+  // Order update date
+  updated: {
+    type: Date,
+    default: null,
+  }
+});
+*/
+
 module.exports = mongoose.model('orders', orderSchema);
