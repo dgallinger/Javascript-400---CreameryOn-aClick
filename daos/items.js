67dc462b57f881f84326bdbb7cbe3b2e1251c24f
supriyapuri <supriyapuri11@gmail.com>
2020-08-28 20:21:58 -0700

    Fixing get All item dao

--- daos/items.js
+++ daos/items.js
@@ -33,11 +33,11 @@ module.exports.create = async (title, price, imagePath, description, story, size
 
  
 
- module.exports.getAll = () => {
+ module.exports.getAll = async() => {
      let itemChunks= [];
     //  console.log(itemChunks);
      console.log("inside get all before adding items");
-    Item.find(function(err, docs) {
+    const allItems = await Item.find(function(err, docs) {
         console.log("Inside find");
         console.log(err);
         let chunkSize = 3;
@@ -46,8 +46,9 @@ module.exports.create = async (title, price, imagePath, description, story, size
         }
         console.log(itemChunks);
         
-        return itemChunks
+        return itemChunks;
  })
+    return allItems;
 };
 
 
