c617063e0b0263bc210767a62296df873dbc8793
supriyapuri <supriyapuri11@gmail.com>
2020-08-28 19:57:41 -0700

    Debugging itemDao getAll

--- daos/items.js
+++ daos/items.js
@@ -34,12 +34,17 @@ module.exports.create = async (title, price, imagePath, description, story, size
  
 
  module.exports.getAll = async(itemChunks) => {
+     console.log(itemChunks);
+     console.log("inside get all before adding items");
     Item.find(function(err, docs) {
-        
+        console.log("Inside find");
+        console.log(err);
         let chunkSize = 3;
         for(let i=0; i < docs.length; i+= chunkSize){
           itemChunks.push(docs.slice(i, i+ chunkSize));
         }
+        console.log(itemChunks);
+        
         return itemChunks
  })
 };
